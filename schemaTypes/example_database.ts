db7bd2dcabbb97579bfe95dbca2dd9a2ce48642e
Helga-Vi <katarina.jorgensen@gmail.com>
2025-04-02 11:29:34 +0200

    defining fields

--- schemaTypes/example_database.ts
+++ schemaTypes/example_database.ts
@@ -2,8 +2,17 @@ export default {
 name: "person",
 title: "Person",
 type: "document",
-fields: [{
+fields: [
+    {
+        name:"fullname",
+        title:"FullName",
+        type: "string",
 
-}
+},
+    {
+        name: "image",
+        title:"Bilde av kaffe",
+        
+    }
 ]
 }
