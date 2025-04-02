03412c2c9682e0b3b23b678b513bfe15c5f83f83
Helga-Vi <katarina.jorgensen@gmail.com>
2025-04-02 11:31:47 +0200

    defined schema

--- schemaTypes/example_database.ts
+++ schemaTypes/example_database.ts
@@ -12,7 +12,15 @@ fields: [
     {
         name: "image",
         title:"Bilde av kaffe",
-        
-    }
-]
+        type: "image",
+        options: {
+            hotspot: true,
+          },
+        },
+        {
+          name: 'price',
+          title: 'Hvor mye koster kaffe?',
+          type: 'number',
+        },
+],
 }
